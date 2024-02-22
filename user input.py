4d857640e67adb0a15539e62643fb73eaf18f634
Sally6753 <159898490+Sally6753@users.noreply.github.com>
2024-02-22 16:31:51 +0300

    Add files via upload

---
+++ user input.py
@@ -0,0 +1,10 @@
+name = ("What is your name? ")
+age =("How old are you? ")
+location = ("Where are you located? ")
+print("Hello {}, {} and {}.".format(name, age, location))
+
+      
+
+
+
+      
